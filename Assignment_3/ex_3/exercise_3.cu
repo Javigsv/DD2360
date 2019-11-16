be5abaa3f3fb2db514b2017705c3016fd52a935c
Miguel Feliu <miguelfeliud@gmail.com>
2019-11-16 16:52:24 +0100

    more comments

--- Assignment_3/ex_3/exercise_3.cu
+++ Assignment_3/ex_3/exercise_3.cu
@@ -260,28 +260,28 @@ int main(int argc, char *argv[])
 	}
 
 	/* set C on GPU and run cublas */
-	//checkCudaErrors(cudaMemset(d_C, 0, sizeof(float)*size*size));
-	//cublas_sgemm(d_C, d_A, d_B, size);
-	
-	/* run naive gpu gemm */
 	/*checkCudaErrors(cudaMemset(d_C, 0, sizeof(float)*size*size));
-	naive_sgemm(d_C, d_A, d_B, size);
-	checkCudaErrors(cudaMemcpy(C_result, d_C, sizeof(float)*size*size, cudaMemcpyDeviceToHost));
-	compare_matrix(C_result, C_truth, size, THRESHOLD);*/
-	
-	/* run shared */
-	checkCudaErrors(cudaMemset(d_C, 0, sizeof(float)*size*size));
-	shared_sgemm(d_C, d_A, d_B, size);
-	checkCudaErrors(cudaMemcpy(C_result, d_C, sizeof(float)*size*size, cudaMemcpyDeviceToHost));
-	compare_matrix(C_result, C_truth, size, THRESHOLD);
+	cublas_sgemm(d_C, d_A, d_B, size);
 	if (verify) {
 		checkCudaErrors(cudaMemcpy(C_result, d_C, sizeof(float)*size*size, cudaMemcpyDeviceToHost));
 		compare_matrix(C_result, C_truth, size, THRESHOLD);
 	}
 	else {
 		checkCudaErrors(cudaMemcpy(C_truth, d_C, sizeof(float)*size*size, cudaMemcpyDeviceToHost));
-	}
-	
+	}*/
+
+	/* run naive gpu gemm */
+	/*checkCudaErrors(cudaMemset(d_C, 0, sizeof(float)*size*size));
+	naive_sgemm(d_C, d_A, d_B, size);
+	checkCudaErrors(cudaMemcpy(C_result, d_C, sizeof(float)*size*size, cudaMemcpyDeviceToHost));
+	compare_matrix(C_result, C_truth, size, THRESHOLD);*/
+
+	/* run shared */
+	checkCudaErrors(cudaMemset(d_C, 0, sizeof(float)*size*size));
+	shared_sgemm(d_C, d_A, d_B, size);
+	checkCudaErrors(cudaMemcpy(C_result, d_C, sizeof(float)*size*size, cudaMemcpyDeviceToHost));
+	compare_matrix(C_result, C_truth, size, THRESHOLD);
+
 	/* free */
 	checkCudaErrors(cudaFree(d_A));
 	checkCudaErrors(cudaFree(d_B));
